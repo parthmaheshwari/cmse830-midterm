b830d20c97eab20d95aa2c5923d30d79df24d6b4
Parth Maheshwari <parthmaheshwari@Parths-MacBook-Pro.local>
2022-10-16 18:48:40 -0400

    Column change

--- global_crisis.py
+++ global_crisis.py
@@ -5,19 +5,19 @@ import seaborn as sns
 import plotly.express as px
 
 data = pd.read_csv("african_crises.csv")
-data = data[["exch_usd", "inflation_annual_cpi", "gdp_weighted_default", "systemic_crisis"]]
-df = data.drop('systemic_crisis', axis=1)
-df_norm = (df-df.min())/(df.max()-df.min())
-df_norm = pd.concat((df_norm, data.systemic_crisis), 1)
+# data = data[["exch_usd", "inflation_annual_cpi", "year", "systemic_crisis"]]
+# df = data.drop('systemic_crisis', axis=1)
+# df_norm = (df-df.min())/(df.max()-df.min())
+# df_norm = pd.concat((df_norm, data.systemic_crisis), 1)
 
 st.write("""
 # Global Crises Data by Country
 How different macroeconomics factor can help us predict systemic crisis in different countries. 
 Shown below: Africa 
 """)
-fig = px.scatter_3d(df_norm, x = 'exch_usd', 
+fig = px.scatter_3d(data, x = 'exch_usd', 
                     y = 'inflation_annual_cpi', 
-                    z = 'gdp_weighted_default',
+                    z = 'year',
                     color = 'systemic_crisis', 
                     size_max = 20, 
                     opacity = 0.5)
