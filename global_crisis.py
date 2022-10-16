e3364a0e7bd786037949d3a3250476bc1ed16900
Parth Maheshwari <parthmaheshwari@Parths-MacBook-Pro.local>
2022-10-16 18:43:02 -0400

    Removed size factor

--- global_crisis.py
+++ global_crisis.py
@@ -5,6 +5,11 @@ import seaborn as sns
 import plotly.express as px
 
 df = pd.read_csv("african_crises.csv")
+
+# df = data.drop('systemic', axis=1)
+# df_norm = (df-df.min())/(df.max()-df.min())
+# df_norm = pd.concat((df_norm, data.species), 1)
+
 st.write("""
 # Global Crises Data by Country
 How different macroeconomics factor can help us predict systemic crisis in different countries. 
@@ -14,7 +19,6 @@ fig = px.scatter_3d(df, x = 'exch_usd',
                     y = 'inflation_annual_cpi', 
                     z = 'gdp_weighted_default',
                     color = 'systemic_crisis', 
-                    size='currency_crises',
                     size_max = 20, 
                     opacity = 0.5)
 
