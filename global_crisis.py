070a2414428315163ed367a637d66be8104bd871
Parth Maheshwari <parthmaheshwari@Parths-MacBook-Pro.local>
2022-10-16 20:10:07 -0400

    Scaling test

--- global_crisis.py
+++ global_crisis.py
@@ -7,16 +7,16 @@ import plotly.express as px
 data = pd.read_csv("african_crises.csv")
 data = data[["exch_usd", "inflation_annual_cpi", "year", "systemic_crisis"]]
 data = data[data["inflation_annual_cpi"]>0]
-df = data.drop('systemic_crisis', axis=1)
-df_norm = (df-df.min())/(df.max()-df.min())
-df_norm = pd.concat((df_norm, data.systemic_crisis), 1)
+# df = data.drop('systemic_crisis', axis=1)
+# df_norm = (df-df.min())/(df.max()-df.min())
+# df_norm = pd.concat((df_norm, data.systemic_crisis), 1)
 
 st.write("""
 # Global Crises Data by Country
 How different macroeconomics factor can help us predict systemic crisis in different countries. 
 Shown below: Africa 
 """)
-fig = px.scatter_3d(df_norm, x = 'exch_usd', 
+fig = px.scatter_3d(data, x = 'exch_usd', 
                     y = 'inflation_annual_cpi', 
                     z = 'year',
                     color = 'systemic_crisis', 
